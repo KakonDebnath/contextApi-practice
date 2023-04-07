f338e9a895000fcae5a53658c4600f4deaec8c95
Kakon Debnath <kakondebnath96@gmail.com>
2023-04-07 22:35:21 +0600

    fix item bg

--- src/components/Sidebar/Item.jsx
+++ src/components/Sidebar/Item.jsx
@@ -5,10 +5,10 @@ import { ProductsContext } from '../Layout/Main';
 const Item = ({item}) => {
     const {handleDeleteItem} = useContext(ProductsContext)
     return (
-        <p className='flex justify-between items-center p-3 px-5 py-1 md:py-3 my-1 md:my-0 rounded-md md:rounded-none bg-white md:bg-lime-300'>
+        <li className='flex justify-between items-center bg-lime-300 p-3 rounded my-1'>
             <span>{item.name}</span>
             <span><TrashIcon onClick={()=>handleDeleteItem(item._id)} className="h-6 w-6 text-red-500 cursor-pointer" /></span>
-        </p>
+        </li>
     );
 };
 
